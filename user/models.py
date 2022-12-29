27b5e20058371cf6e56a13acb7276add1de45d5a
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-12-29 18:47:35 +0900

    Feat: Add ManyToManyField for User/Theme Models

--- user/models.py
+++ user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
+from theme.models import Theme
+
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -34,14 +36,12 @@ class User(AbstractBaseUser):
 
     username = models.CharField(max_length=200, unique=True)
     realname = models.CharField(max_length=20)
-
     nickname = models.CharField(max_length=50, unique=True, blank=True, null=True) # 추후에 api post 요청으로 닉네임 받아와야함
-
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
-
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    saved_themes = models.ManyToManyField(Theme, related_name='saved_users')
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['realname', ]
