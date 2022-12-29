e2c99081b059e2904778c5cc1690d301ceebcc13
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-12-29 18:12:10 +0900

    Refactor: Remove spaces between codes

--- theme/models.py
+++ theme/models.py
@@ -12,15 +12,11 @@ class ThemeType(models.Model):
 
 class Theme(models.Model):
     theme_type = models.ForeignKey(ThemeType, on_delete=models.SET_NULL, null=True)
-
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-
     recipe_count = models.IntegerField(default=0)
     duration = models.IntegerField(default=0)
-
     tips = models.URLField(blank=True, null=True)
-
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
