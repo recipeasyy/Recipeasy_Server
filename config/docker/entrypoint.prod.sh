2ae4d71b3f6446690bab16936a6ec377896669dc
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2022-12-30 17:38:08 +0900

    Fix: Fix database error

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+echo "Collect static files"
 python manage.py collectstatic --no-input
 
+echo "Apply database migrations"
 python manage.py makemigrations
-
 python manage.py migrate
 
 exec "$@"
