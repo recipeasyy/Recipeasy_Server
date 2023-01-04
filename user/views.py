ca6af2416a7988c2cbdf04f118ecb316964b7bb4
bokdol11859 <2019147551@yonsei.ac.kr>
2023-01-04 21:22:32 +0900

    Fix: Remove login endpoint

--- user/views.py
+++ user/views.py
@@ -13,13 +13,6 @@ from user.models import User
 from user.serializer import UserSerializer
 from user.utils import get_tokens_for_user
 
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def login_url_view(request):
-    return Response({"URL": f"https://kauth.kakao.com/oauth/authorize?client_id={env('KAKAO_CLIENT_ID')}&redirect_uri={env('KAKAO_REDIRECT_URI')}&response_type=code"})
-
-
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def kakao_login_view(request):
