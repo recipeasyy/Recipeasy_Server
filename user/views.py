31edbb780cfedc340d0c718a4d7a6c53bc2ed868
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-01-05 16:26:15 +0900

    Fix: Fix Redirect URI error

--- user/views.py
+++ user/views.py
@@ -19,7 +19,6 @@ def kakao_login_view(request):
 
     code = request.GET.get('code', None)
 
-    print(code)
 
     headers = {
         'Access-Control-Allow-Origin': f'{env("API_ENDPOINT")}',
@@ -38,7 +37,6 @@ def kakao_login_view(request):
     token_req = requests.post(url, headers=headers, data=data)
     token_req_json = token_req.json()
     access_token = token_req_json.get("access_token")
-    print(access_token)
     kakao_api_response = requests.post(
         "https://kapi.kakao.com/v2/user/me",
         headers={
@@ -49,10 +47,8 @@ def kakao_login_view(request):
     ).json()
 
     user_id = kakao_api_response.get('id')
-    print(user_id)
     username = f'Recipeasy-{user_id}'
     realname = kakao_api_response.get('properties').get('nickname')
-    print(realname)
     try:
         user = User.objects.get(username=username)
 
@@ -60,7 +56,6 @@ def kakao_login_view(request):
         user = User(username=username, realname=realname)
         user.save()
         user = User.objects.get(username=username)
-    print(user)
     token = get_tokens_for_user(user)
 
     data = {'realname': realname, 'username': username, 'access_token': token['access'], 'refresh_token': token['refresh']}
